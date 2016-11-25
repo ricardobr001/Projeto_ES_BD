52cc5ab7c0cd970c91c3d6e3b15cee2c164228c0
Ricardo Mendes <ricardomlealjr@gmail.com>
2016-11-25 17:40:03 -0200

    Classes funcionário criada, precisando terminar

---
+++ projeto/model/FuncionarioContratado.php
@@ -0,0 +1,184 @@
+<?php
+  require_once dirname(__FILE__) . '/../conexao/Connection.php';
+
+  class FuncionarioContratado extends Funcionario{
+    private $dataEntrada;
+    private $cargo;
+    private $qtddHorasTrabalhadas;
+    private $dataSaida;
+    private $salario;
+
+    //Construtor da classe
+    public function __construc($codigo, $nome, $CPF, $dataNascimento, $telefone, $estado, $motivo, $periodo, $setor, $terminal, $cidade, $rua, $bairro, $CEP, $numero, $complemento, $dataEntrada, $cargo, $qtddHorasTrabalhadas, $dataSaida, $salario){
+      $this->codigoDoFuncionario = $codigo;
+      $this->nome = $nome;
+      $this->CPF = $CPF;
+      $this->dataNascimento = $dataNascimento;
+      $this->telefone = $telefone;
+      $this->estado = $estado;
+      $this->motivo = $motivo;
+      $this->periodo = $periodo;
+      $this->setor = $setor;
+      $this->terminal = new Terminal($terminal);
+      $this->endereco = new Endereco($cidade, $rua, $bairro, $CEP, $numero, $complemento);
+      $this->dataEntrada = $dataEntrada;
+      $this->cargo = $cargo;
+      $this->qtddHorasTrabalhadas = $qtddHorasTrabalhadas;
+      $this->dataSaida = $dataSaida;
+      $this->salario = $salario;
+    }
+
+    //Getters e Setters
+    public function setDataEntrada($dataEntrada){
+        $this->dataEntrada = $dataEntrada;
+
+        return $this;
+    }
+
+    public function getDataEntrada(){
+        return $this->dataEntrada;
+    }
+
+    public function setCargo($cargo){
+        $this->cargo = $cargo;
+
+        return $this;
+    }
+
+    public function getCargo(){
+        return $this->cargo;
+    }
+
+    public function setQtddHorasTrabalhadas($qtddHorasTrabalhadas){
+        $this->qtddHorasTrabalhadas = $qtddHorasTrabalhadas;
+
+        return $this;
+    }
+
+    public function getQtddHorasTrabalhadas(){
+        return $this->qtddHorasTrabalhadas;
+    }
+
+    public function setDataSaida($dataSaida){
+        $this->dataSaida = $dataSaida;
+
+        return $this;
+    }
+
+    public function getDataSaida(){
+        return $this->dataSaida;
+    }
+
+    public function setSalario($salario){
+        $this->salario = $salario;
+
+        return $this;
+    }
+
+    public function getSalario(){
+        return $this->salario;
+    }
+
+    //Salvando os dados da classe no banco
+    public function cadastrar(){
+      $conn = Connection::open();
+
+      if(!conn){
+        $msg = 'Problemas na conexão';
+      }
+      else{
+        mysqli_query($conn, /*INSTRUÇÃO MYSQL PARA INSERIR OS DADOS NO BANCO*/);
+        Connection::close($conn);
+        $msg = 'Funcionário cadastrado com sucesso!';
+      }
+
+      return $msg;
+    }
+
+    //Buscando uma informação no banco
+    public function buscar($codigoDoFuncionario){
+      $conn = Connection::open();
+
+      if(!conn){
+        $msg = 'Problemas na conexão';
+      }
+      else{
+        /*IMPLEMENTAR A RECUPERAÇÃO OS DADOS NO BANCO*/
+
+      }
+
+    }
+
+    //Alterando os dados de um funcionário no banco
+    public function alterarDados(){
+      $conn = Connection::open();
+
+      if(!conn){
+        $msg = 'Problemas na conexão';
+      }
+      else{
+      mysqli_query($conn, /*INSTRUÇÃO MYSQL PARA ALTERAR OS DADOS NO BANCO*/);
+        Connection::close($conn);
+        $msg = 'Dados alterados com sucesso!';
+      }
+
+      return $msg;
+    }
+
+    //Alterando o status de um funcionário no banco
+    public function alterarStatus(){
+      $conn = Connection::open();
+
+      if(!conn){
+        $msg = 'Problemas na conexão';
+      }
+      else{
+      mysqli_query($conn, /*INSTRUÇÃO MYSQL PARA ALTERAR O STATUS NO BANCO*/);
+        Connection::close($conn);
+        $msg = 'Status alterado com Sucesso!';
+      }
+
+      return $msg;
+    }
+
+    //Gerando o relatório
+    public function gerarRelatorioPorFuncao($setor){
+      $conn = Connection::open();
+
+      if(!conn){
+        $msg = 'Problemas na conexão';
+      }
+      else{
+      /*IMPLEMENTAR A RECUPERAÇÃO OS DADOS NO BANCO E GERAR O RELATÓRIO*/
+
+      }
+
+    }
+
+    public function gerarRelatorioPorTerminal($terminal){
+      $conn = Connection::open();
+
+      if(!conn){
+        $msg = 'Problemas na conexão';
+      }
+      else{
+      /*IMPLEMENTAR A RECUPERAÇÃO OS DADOS NO BANCO E GERAR O RELATÓRIO*/
+
+      }
+
+
+    }
+
+    public function gerarRelatorioPorFaixaSalarial($min, $max){
+      $conn = Connection::open();
+
+      if(!conn){
+        $msg = 'Problemas na conexão';
+      }
+      else{
+      /*IMPLEMENTAR A RECUPERAÇÃO OS DADOS NO BANCO E GERAR O RELATÓRIO*/
+
+      }
+    }
+}
+?>
---
+++ projeto/model/FuncionarioTerceirizado.php
@@ -0,0 +1,24 @@
+<?php
+  require_once dirname(__FILE__) . '/../conexao/Connection.php';
+
+  class FuncionarioTerceirizado extends Funcionario{
+    //Atributos herdados da classe funcionário!!
+
+    //Construtor da classe
+    public function __construc($codigo, $nome, $CPF, $dataNascimento, $telefone, $estado, $motivo, $periodo, $setor, $terminal, $cidade, $rua, $bairro, $CEP, $numero, $complemento){
+      $this->codigoDoFuncionario = $codigo;
+      $this->nome = $nome;
+      $this->CPF = $CPF;
+      $this->dataNascimento = $dataNascimento;
+      $this->telefone = $telefone;
+      $this->estado = $estado;
+      $this->motivo = $motivo;
+      $this->periodo = $periodo;
+      $this->setor = $setor;
+      $this->terminal = new Terminal($terminal);
+      $this->endereco = new Endereco($cidade, $rua, $bairro, $CEP, $numero, $complemento);
+    }
+
+    //Metodos herdados da classe funcionário!!
+}
+?>
