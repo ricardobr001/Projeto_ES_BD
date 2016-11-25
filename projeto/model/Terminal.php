e2112c9d0e79c92415824cadf92bc669670be818
Ricardo Mendes <ricardomlealjr@gmail.com>
2016-11-25 17:54:00 -0200

    Classe Terminal implementada

---
+++ projeto/model/Terminal.php
@@ -0,0 +1,40 @@
+<?php
+  class Terminal{
+    private $codigoDoTerminal;
+    private $nome;
+    private $localizacao;
+
+    //Contrutor da classe
+    public function __construc($terminal){
+      $this->nome = $terminal;
+      $this->codigo = 1;            //Como cadastros de terminal não é o escopo do nosso grupo, simularemos os dados para o nosso projeto
+      $this->localizacao = 'oeste';
+    }
+
+    //Getters e Setters
+    public function setCodigoDoTerminal($codigoDoTerminal){
+        $this->codigoDoTerminal = $codigoDoTerminal;
+    }
+
+    public function getCodigoDoTerminal(){
+        return $this->codigoDoTerminal;
+    }
+
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function setLocalizacao($localizacao){
+        $this->localizacao = $localizacao;
+    }
+
+    public function getLocalizacao(){
+        return $this->localizacao;
+    }
+
+}
+?>
