58ea56bd83d12b4b9c67cbbb435018532dbf3989
Ricardo Mendes <ricardomlealjr@gmail.com>
2016-11-26 20:29:55 -0200

    precisa arrumar para inserir no banco

--- projeto/model/FuncionarioTerceirizado.php
+++ projeto/model/FuncionarioTerceirizado.php
@@ -19,6 +19,60 @@
       $this->endereco = new Endereco($cidade, $rua, $bairro, $CEP, $numero, $complemento);
     }
 
+    //Salvando os dados da classe no banco
+    public function cadastrar($CNPJ){
+      $conn = Connection::open();
+
+      if(!conn){
+        $msg = 'Problemas na conexão';
+      }
+      else{
+        if(!mysqli_query($conn, "INSERT INTO funcionario(
+          codigo_funcionario,
+          nome,
+          cpf,
+          data_nascimento,
+          telefone,
+          estado,
+          motivo,
+          cargo,
+          periodo,
+          terminal,
+          setor,
+          cidade,
+          rua,
+          bairro,
+          numero,
+          complemento,
+          cep,
+          cnpj_empresa
+        ) VALUES (
+          '".$this->codigoDoFuncionario."',
+          '".$this->nome."',
+          '".$this->CPF."',
+          '".$this->dataNascimento."',
+          '".$this->telefone."',
+          '".$this->estado."',
+          '".$this->motivo."',
+          'TERCEIRIZADO',
+          '".$this->periodo."',
+          '".$this->terminal->getNome()."',
+          '".$this->setor."',
+          '".$this->endereco->getCidade()."',
+          '".$this->endereco->getRua()."',
+          '".$this->endereco->getBairro()."',
+          '".$this->endereco->getNumero()."',
+          '".$this->endereco->getComplemento()."',
+          '".$this->endereco->getCEP()."',
+          '".$CNPJ."'
+        );"))
+           die(mysqli_error($conn));
+        Connection::close($conn);
+        $msg = 'Funcionário cadastrado com sucesso!';
+      }
+
+      return $msg;
+    }
     //Metodos herdados da classe funcionário!!
 }
 ?>
