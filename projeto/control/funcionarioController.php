cb8b51b2cb6ad3d396331750b1a290a781805893
Ricardo Mendes <ricardomlealjr@gmail.com>
2016-11-29 16:51:13 -0200

    Criado o controller para Funcionario

---
+++ projeto/control/funcionarioController.php
@@ -0,0 +1,24 @@
+<?php
+	require_once dirname(__FILE__) .'/../model/FuncionarioContratado.php';
+	require_once dirname(__FILE__) .'/../model/Funcionario.php';
+	require_once dirname(__FILE__) .'/../model/Endereco.php';
+	require_once dirname(__FILE__) .'/../model/Terminal.php';
+	spl_autoload_register(function ($class_name) {
+    include $class_name . '.php';
+});
+
+	$pagina = $_POST["pagina"];
+
+	switch ($pagina){
+		case "alterar_status":
+			$codigo = $_POST["codigo"];
+      $motivo = $_POST["motivo"];
+      $status = $_POST["status"];
+
+      $funcionario = new Funcionario($codigo, "", "", "", "", $estado, $motivo, "", "", "", "", "", "", "", "", "");
+      $msg = $funcionario->alterarStatus();
+
+      echo $msg;
+		break;
+	}
+?>
